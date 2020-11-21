857b0f915ac340f69a45e85e26498e27af9dfe0c
francispastore <48254276+francispastore@users.noreply.github.com>
2020-11-20 22:20:32 -0500

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,27 @@
-
-
 def encrypt_line(line, key):
-    pass
+
+
+array1 = []
+
+    for i in line:
+	
+		if i.isupper():
+		
+			line + key % 26
+			
+		if i.islower():
+		
+			line + key % 26
+		
+		if i.ispunct():
+		
+			line + key % 19 #need to check values for key encryption
+			
+		if i.isdigit():
+		
+			line + key % 10
+		
+	
+	return line #need to put this into an array
+	
+	encrypt_line()
