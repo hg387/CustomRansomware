5341f0e9360e4c217ff2f40a622b6058fba8dd0c
francispastore <48254276+francispastore@users.noreply.github.com>
2020-11-20 22:26:41 -0500

    Add files via upload

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,26 @@
-
 def decrypt_line(line, key):
-    pass
+
+array1 = []
+
+    for i in line:
+	
+		if i.isupper(): #for uppercase characters A-Z
+		
+			line - key % 26
+			
+		if i.islower(): #for lowercase characters a-z
+		
+			line - key % 26
+		
+		if i.ispunct(): #for punctuation !@#$%^&*()-_=+`~[]\{}|;':",./<>?
+		
+			line - key % 32 #need to check values for key encryption
+			
+		if i.isdigit(): #for digits 0-9
+		
+			line - key % 10
+		
+	
+	return line #need to put this into an array
+	
+	decrypt_line()
